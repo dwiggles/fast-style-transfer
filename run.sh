d321a5822fd9110e35944fd102f04a02a7e4d110
Dan Wigglesworth <dwiggles@kewbeach.ca>
2018-09-29 23:37:24 -0400

    Added the command line now
    The previous mod only commented out the sytle-a-vid command. This one adds the style-an-image comment.

--- run.sh
+++ run.sh
@@ -23,3 +23,7 @@ mkdir data/bin
 #  --device /gpu:0 \
 #  --batch-size 4 2>&1
 
+#run style transfer on a single image -- adapted from the example in the README.md file 2018.09.29
+python evaluate.py --checkpoint ./scream.ckpt \
+  --in-path examples/WhatsApp\ Image\ 2018-05-10\ at\ 3.34.49\ PM.jpeg \
+  --out-path /artifacts/new.jpg
