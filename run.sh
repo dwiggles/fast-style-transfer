3dabdc8e83814b55b94ac0339bd88d8f3bc53f64
Dan Wigglesworth <dwiggles@kewbeach.ca>
2018-09-30 20:54:44 -0400

    diags

--- run.sh
+++ run.sh
@@ -21,12 +21,14 @@ mkdir data/bin
 mkdir data
 cd data
 pwd
+echo Next: which wget
 which wget
+echo Next: which unzip
 which unzip
-/usr/bin/local/wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+/usr/bin/wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 /usr/bin/wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-/usr/local/bin/unzip train2014.zip
+/usr/bin/unzip train2014.zip
 cd ..
 pwd
 
