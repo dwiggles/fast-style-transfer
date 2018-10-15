a7dc6e13f3a86b90e58998087361f84d9fcfacf8
Dan Wigglesworth <dwiggles@kewbeach.ca>
2018-10-15 08:53:09 -0400

    reverse the crazy

--- run.sh
+++ run.sh
@@ -33,7 +33,7 @@ apt-install || exit 1
 pwd
 
 # step2 2: train
-/usr/bin/sudo python style.py --style examples/style/wave.jpg \
+python style.py --style examples/style/wave.jpg \
   --checkpoint-dir ./ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
